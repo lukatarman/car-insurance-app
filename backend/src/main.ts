e2a9924552acf0724fc41707a553d0193b792c3c
Luka Tarman <luka.tarman@gmail.com>
2023-12-22 16:24:14 +0100

    project setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,11 @@
 import { User } from "./models/user.ts";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 console.log("started");
 
 const testUser = new User("Luka Tarman", new Date("21 september 1989"), "Ljubljana", 500);
 
+const database = async () => await MongoMemoryServer.create();
+
+console.log(database);
 console.log(testUser);
