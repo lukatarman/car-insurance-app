020a536fa650c73f324f870fde2e82f9d2ff4e4e
Luka Tarman <luka.tarman@gmail.com>
2023-12-23 10:27:08 +0100

    installed ts node and typescript

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { User } from "./models/user.ts";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
-console.log("started");
+const main = async (): Promise<void> => {
+  console.log("started");
 
-const testUser = new User("Luka Tarman", new Date("21 september 1989"), "Ljubljana", 500);
+  const testUser = new User(
+    "Luka Tarman",
+    new Date("21 september 1989"),
+    "Ljubljana",
+    500
+  );
 
-const database = async () => await MongoMemoryServer.create();
+  const databaseServer = await MongoMemoryServer.create();
 
-console.log(database);
-console.log(testUser);
+  console.log(databaseServer.getUri());
+  console.log(testUser);
+  databaseServer.stop();
+};
+
+main();
