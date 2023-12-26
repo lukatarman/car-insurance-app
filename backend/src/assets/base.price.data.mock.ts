75d30236bf37eae415bbd77c644121440150bf23
Luka Tarman <luka.tarman@gmail.com>
2023-12-26 17:27:27 +0100

    added base price data mock

---
+++ backend/src/assets/base.price.data.mock.ts
@@ -0,0 +1,14 @@
+export const cityPrices = [
+  {
+    location: "rijeka",
+    price: 150,
+  },
+  { location: "zagreb", price: 220 },
+  { location: "osijek", price: 130 },
+];
+
+export const ageSurcharges = [
+  { maxAge: 25, percentageSurcharge: 20 },
+  { maxAge: 35, percentageSurcharge: 10 },
+  { maxAge: 45, percentageSurcharge: 5 },
+];
