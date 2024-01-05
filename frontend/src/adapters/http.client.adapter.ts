a7bc0f781a91d965add734a6838b8ba5e71d24ef
Luka Tarman <luka.tarman@gmail.com>
2024-01-05 21:25:22 +0100

    added methods to adapter

--- frontend/src/adapters/http.client.adapter.ts
+++ frontend/src/adapters/http.client.adapter.ts
@@ -1,7 +1,41 @@
 import httpClient from "../client/http.client";
-import { User } from "../models/users.js";
+import { User } from "../models/users";
+import { CoverageType } from "../types/index";
 
 export async function addUser(data: User) {
-  const response = await httpClient.post(`/users`, data);
-  console.log(`Response from server: ${response.data}`);
+  try {
+    const response = await httpClient.post(`/users`, data);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getUserByName(name: string) {
+  try {
+    const response = await httpClient.get(`/users/${name}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function changeCoverageStatus(name: string, data: CoverageType) {
+  try {
+    const response = await httpClient.put(`/users/${name}`, data);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function changeDiscountSelectionStatus(
+  name: string,
+  coverageName: CoverageType
+) {
+  try {
+    await httpClient.put(`/users/${name}/discounts`, coverageName);
+  } catch (err) {
+    console.log(err);
+  }
 }
